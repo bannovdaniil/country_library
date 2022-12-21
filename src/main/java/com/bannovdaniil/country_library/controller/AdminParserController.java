f4a8dd7c2495f4f9fad0eca719ad30b17cc2ab77
bannovdaniil <baddan@mail.ru>
2022-12-21 23:15:19 +0300

    !7 clear import

--- src/main/java/com/bannovdaniil/country_library/controller/AdminParserController.java
+++ src/main/java/com/bannovdaniil/country_library/controller/AdminParserController.java
@@ -1,11 +1,8 @@
 package com.bannovdaniil.country_library.controller;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,8 +12,7 @@ public class AdminParserController {
 
     @GetMapping("/parser")
     public String searchCountyByName() {
-        log.info("admin Parse: {}");
+        log.info("admin Parse: ");
         return null;
     }
-
 }
